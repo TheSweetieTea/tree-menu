29dba69995b54bf3f0cb4ef4ccf60f94c5e41c8d
Akbar Alijonov <akbaralizhonov@mail.ru>
2024-04-21 00:15:31 +0300

    Add test and update view

--- tree_menu/menu_app/tests.py
+++ tree_menu/menu_app/tests.py
@@ -5,6 +5,6 @@ from django.test import TestCase
 
 class MenuItemTestCase(TestCase):
     def test_status_code(self):
-        response = self.client.get(reverse("menu_app:menu_view"))
+        response = self.client.get(reverse("menu_app:main_menu"))
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
